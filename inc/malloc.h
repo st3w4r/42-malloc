67544d0a5a5ccc1627851c3899e171c0f0ef520e
Yanael Barbier <st3w4r@gmail.com>
2017-05-03 14:04:28 +0200

    Add files

---
+++ inc/malloc.h
@@ -0,0 +1,12 @@
+#ifndef MALLOC_H
+# define MALLOC_H
+
+# include <sys/mman.h>
+
+# define SMALL 1000
+# define LARGE 10000
+
+int allocation;
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,57 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <stdint.h>
 
-int main() {
-	printf("Hello");
-	return 0;
+int main(int argc, char **argv) {
+
+	int i;
+	int nb;
+	char c;
+	char *str;
+	int page_size;
+
+	nb = atoi(argv[2]);
+	c = argv[1][0];
+	str = (char*)mmap(0, nb + 1, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+
+	i = 0;
+
+	while(i<nb) {
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+	printf("%s\n", str);
+	page_size = getpagesize();
+	printf("%d\n", page_size);
+
+	int success;
+	struct rlimit *rlimit;
+
+	rlimit = malloc(sizeof(struct rlimit));
+
+	// printf("Limit: %lu\n", sizeof(struct rlimit));
+
+	success = getrlimit(RLIMIT_NPROC, rlimit);
+
+	printf("Core limit soft: %ju\n", (uintmax_t)rlimit->rlim_cur);
+	printf("Core limit soft: %juMB\n", (uintmax_t)rlimit->rlim_cur / 1024 / 1024);
+	printf("Core limit hard: %ju\n", (uintmax_t)rlimit->rlim_max);
+
+	return (0);
 }
+
+// void *malloc(size_t size) {
+//
+// }
+//
+// void free(void *ptr) {
+//
+// }
+//
+// void *realloc(void *ptr, size_t size) {
+//
+// }
